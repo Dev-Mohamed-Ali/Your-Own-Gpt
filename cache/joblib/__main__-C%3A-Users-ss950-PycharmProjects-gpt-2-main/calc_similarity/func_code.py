2d5a8f338ef17ee839de24bf84db71ac3fd90f27
ss950 <meen79508@gmail.com>
2023-07-30 19:26:15 +0300

    fix cach

--- cache/joblib/__main__-C%3A-Users-ss950-PycharmProjects-gpt-2-main/calc_similarity/func_code.py
+++
@@ -1,6 +0,0 @@
-# first line: 15
-@memory.cache
-def calc_similarity(user_question, question):
-    embeddings_user_question = model.encode(user_question, convert_to_tensor=True)
-    embeddings_question = model.encode(question, convert_to_tensor=True)
-    return util.pytorch_cos_sim(embeddings_user_question, embeddings_question)[0, 0]
